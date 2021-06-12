29a089a068a9b613e4054977a2705760714b0ad4
Hare-Ball <robby.hare@gmail.com>
2021-06-11 20:23:09 -0500

    updates to JS with API info

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+//Jailbase API
+
+// Here is the link to the documentation 
+// https://www.jailbase.com/api/
+// There are multiple methods, but the only one I think we will need is the search one.
+
+// Request from https://www.jailbase.com/api/1/search/?last_name= "Name search"
+
+// Last name is required.
+// We could also add first name, state, and county to the search form
+
+// FBI Most Wanted API
+
+// Documentation is sparse on this one, but here is the link:
+// https://www.fbi.gov/wanted/api
+
+// Request from https://api.fbi.gov/wanted/v1/list 
+
