3268bfe65c8db81a90a2fad475f27e9c84a4cfce
Hare-Ball <robby.hare@gmail.com>
2021-06-18 17:08:39 -0500

    changes to HTML and functions to append quote to the correct place on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,43 @@
-var dateInfo = document.getElementById("date_info");
-var firstName = document.getElementById("first_name");
-var lastName = document.getElementById("last_name");
-var state = document.getElementById("state");
-var nextBtn = document.getElementById("next_btn");
-var submitBtn2 = document.getElementById("submit_btn2");
-var resultName = document.getElementById("date_name");
-var results = document.getElementById("criminal_container");
-var errorPop = document.getElementById("error_modal");
+// var dateInfo = document.getElementById("date_info");
+// var firstName = document.getElementById("first_name");
+// var lastName = document.getElementById("last_name");
+// var state = document.getElementById("state");
+ var nextBtn = document.getElementById("next_btn");
+// var submitBtn2 = document.getElementById("submit_btn2");
+// var resultName = document.getElementById("date_name");
+// var results = document.getElementById("criminal_container");
+// var errorPop = document.getElementById("error_modal");
 
-console.log(lastName);
+// console.log(lastName);
 
-function gotoTranslated(event) {
-  event.preventDefault();
-  console.log("clicked")
-  document.getElementById("form").classList.add("hide");
-  document.getElementById("results-page").classList.remove("hide"); 
-  // dataSubmit();
-}
+ function gotoTranslated(event) {
+   event.preventDefault();
+   console.log("clicked")
+   document.getElementById("form").classList.add("hide");
+   document.getElementById("results-page").classList.remove("hide"); 
+//   // dataSubmit();
+ }
 
-function pullResults() {
-  const queryURL = `https://www.jailbase.com/api/1/search/?source_id=${source_id}&first_name=${firstName}&last_name=${lastName}`;
-  //check documentation for API
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-    success: displayResult(data),
-    success: displayResult,
-  });
-}
+// function pullResults() {
+//   const queryURL = `https://www.jailbase.com/api/1/search/?source_id=${source_id}&first_name=${firstName}&last_name=${lastName}`;
+//   //check documentation for API
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET",
+//     success: displayResult(data),
+//     success: displayResult,
+//   });
+// }
 
 function displayResult(data) {
   console.log(data);
   //append the results from API call to var results
 }
 
-nextBtn.addEventListener("click", gotoTranslated);
-submitBtn2.addEventListener("click", gotoSearchResults);
+
 
 //ADDING THE QUOTE TO THE PAGE 
-var quoteEl = $('#submit_btn');
+var quoteEl = $('#next_btn');
 
 function getQuote() {
   const queryURL = 'https://free-quotes-api.herokuapp.com/';
@@ -48,65 +47,24 @@ function getQuote() {
     success: function(data){
       console.log(data)
       console.log(data.quote + data.author)
-      var quote = $("#og-Quote")//add quote area ID
-      quote.append(data.quote + " - " + data.author)
+      var ogQuote = $("#og-Quote")//add quote area ID
+      ogQuote.append(data.quote)
+      var ogAuthor =$("#og-author")
+      ogAuthor.append("~"+data.author)
+      var trAuthor= $("#tr-author")
+      trAuthor.append("~"+data.author)
     }
   })
 }
 
-function getSourceIds() {
-  const queryURL = "https://www.jailbase.com/api/1/sources/";
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-    success: function (data) {
-      console.log(data);
-      var stateArray = data.records;
-      for (i = 0; i < stateArray.length; i++) {
-        if (stateArray[i].state_full) {
-          console.log(stateArray[i].state_full);
-
-          stateName.text();
-        }
-      }
-      var countyArray = data.records;
-      for (i = 0; i < countyArray.length; i++) {
-        if (countyArray[i].name) {
-          console.log(countyArray[i].name);
-        }
-      }
-    },
-  });
-}
-
 quoteEl.click(getQuote);
 
-var ogQuote = localStorage.getItem();
+//var ogQuote = localStorage.getItem();
 
-// function getquote() {
-//   const queryURL = "https://www.jailbase.com/api/1/sources/";
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET",
-//     success: function (data) {
-//       console.log(data);
-//       var stateArray = data.records;
-//       for (i = 0; i < stateArray.length; i++) {
-//         if (stateArray[i].state_full) {
-//           console.log(stateArray[i].state_full);
+nextBtn.addEventListener("click", gotoTranslated);
+//submitBtn2.addEventListener("click", gotoSearchResults);
 
-//           stateName.text();
-//         }
-//       }
-//       var countyArray = data.records;
-//       for (i = 0; i < countyArray.length; i++) {
-//         if (countyArray[i].name) {
-//           console.log(countyArray[i].name);
-//         }
-//       }
-//     },
-//   });
-// }
+
 
 // PARSE the data
 // (same as doing a for loop and looping over the array)
@@ -117,4 +75,3 @@ var ogQuote = localStorage.getItem();
 // return countyOffices[whateveryouwant.state_full].push(whateveryouwant);
 //})
 
-getSourceIds();
